ef7f3f7cadd3f6340eb9602220086f80fb9afbfa
judithnl <92431648+judithnl@users.noreply.github.com>
2022-03-26 17:14:43 +0100

    a1

--- l1zufallsgedicht/GedichtScript.js
+++ l1zufallsgedicht/GedichtScript.js
@@ -1,2 +1,19 @@
-console.log("hallo");
+var Names = ["Harry", "Hermine", "Ron", "Hagrid", "Snape", "Dumbledore"];
+var Predicats = ["braut", "liebt", "studiert", "hasst", "zaubert", "zerstört"];
+var Objects = ["Zaubertränke", "den Grimm", "Lupin", "Hogwarts", "die Karte des Rumtreibers", "Dementoren"];
+for (var i = Objects.length; i >= 1; i--) {
+    var outcome = getVerse(Names, Predicats, Objects);
+    console.log(outcome);
+}
+function getVerse(_names, _predicats, _objects) {
+    var verse = "";
+    var RandomNumber;
+    RandomNumber = Math.floor(Math.random() * Objects.length);
+    verse = verse + _names.splice(RandomNumber, 1)[0] + " ";
+    var RandomNumber2 = Math.floor(Math.random() * _predicats.length);
+    verse = verse + _predicats.splice(RandomNumber2, 1)[0] + " ";
+    var RandomNumber3 = Math.floor(Math.random() * _objects.length);
+    verse = verse + _objects.splice(RandomNumber3, 1)[0] + " ";
+    return verse;
+}
 //# sourceMappingURL=GedichtScript.js.map
--- l1zufallsgedicht/GedichtScript.ts
+++ l1zufallsgedicht/GedichtScript.ts
@@ -1,1 +1,28 @@
-console.log("hallo")
+let Names:string[]=["Harry", "Hermine", "Ron", "Hagrid", "Snape", "Dumbledore"]
+let Predicats:string[]=["braut","liebt","studiert","hasst","zaubert","zerstört"]
+let Objects:string[]=["Zaubertränke","den Grimm","Lupin","Hogwarts","die Karte des Rumtreibers","Dementoren"]
+
+
+
+
+for(let i=Objects.length;i>=1;i--){
+    let outcome=getVerse(Names,Predicats,Objects)
+    console.log(outcome)
+}
+
+
+function getVerse(_names:string[], _predicats:string[],_objects:string[]):string{
+    let verse:string="";
+
+    let RandomNumber:number;
+    RandomNumber = Math.floor(Math.random() * Objects.length)
+
+    verse = verse + _names.splice(RandomNumber, 1)[0] + " ";
+    let RandomNumber2 = Math.floor(Math.random() * _predicats.length);
+    verse = verse + _predicats.splice(RandomNumber2, 1)[0] + " ";
+    let RandomNumber3 = Math.floor(Math.random() * _objects.length);
+    verse = verse + _objects.splice(RandomNumber3, 1)[0] + " ";
+
+     return verse;
+}
+
